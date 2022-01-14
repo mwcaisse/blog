78a302dec6160fad60353af96a8cb081f574c6ae
Mitchell Caisse <mitchell@mwcaisse.com>
2022-01-13 21:45:23 -0500

    Added drone configuration

---
+++ ci-scripts/wait-for-blog-to-start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+attempt_counter=0
+max_attempts=20
+
+until $(curl --output /dev/null --silent --head --fail http://localhost:8080); do
+    if [[ ${attempt_counter} -eq ${max_attempts} ]]; then
+        echo "Max attempts reached, aborting!"
+        exit 1
+    fi
+
+    printf "."
+    attempt_counter=$(($attempt_counter + 1))
+    sleep 5
+done
