add30cf6529a1d8452171e1a832bc074a848a633
Mitchell Caisse <mitchell@mwcaisse.com>
2022-01-13 22:09:46 -0500

    Update path to check up for blog..

--- ci-scripts/wait-for-blog-to-start.sh
+++ ci-scripts/wait-for-blog-to-start.sh
@@ -3,7 +3,7 @@
 attempt_counter=0
 max_attempts=20
 
-until $(curl --output /dev/null --head --fail http://localhost:8080); do
+until $(curl --output /dev/null --head --fail http://blog:8080); do
     if [[ ${attempt_counter} -eq ${max_attempts} ]]; then
         echo "Max attempts reached, aborting!"
         exit 1
